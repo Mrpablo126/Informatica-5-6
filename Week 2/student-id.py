842d8b06171dd23bde4f44867af6d1f1ac9d8c0c
Rodrigo <rlopezca89@gmail.com>
2025-08-20 20:21:45 +0000

    class 8

---
+++ Week 2/student-id.py
@@ -0,0 +1,15 @@
+
+print('')
+print("Please enter the following information to create your ID Card.")
+input("Press enter to continue.")
+
+name = input("First Name:")
+last = input("Last Name:")
+email = input("Email:")
+phone = input("Phone Number:")
+id = input("ID Number:")
+clas = input("Choose your class:")
+
+line = "-" * 40
+print(line)
+print(last.upper), print(name.title)

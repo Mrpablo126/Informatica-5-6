0f1a72c5d8de5a1572cf6ee9458bc8d49880eab8
Rodrigo <rlopezca89@gmail.com>
2025-09-01 19:32:17 +0000

    class 18

---
+++ Week 4/adv-strings.py
@@ -0,0 +1,39 @@
+# #String Length 
+input_string = input("Please type a message:")
+
+# print("-" * len(input_string))
+
+# #String Index
+
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[2])
+# print(input_string[3])
+# print(input_string[4])
+# print(input_string[5])
+# print(input_string[6])
+# print(input_string[7])
+# print(input_string[8])
+# print(input_string[9])
+# # print(input_string[10])
+# print(input_string[11])
+# print(input_string[12])
+# print(input_string[13])
+# print(input_string[14])
+# print(input_string[15])
+# print(input_string[16])
+# print(input_string[17])
+# print(input_string[18])
+# print(input_string[19])
+
+#Example
+#i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+#Example 2
+i = -1
+while i < len(input_string):
+    print(input_string[i])
+    i -= 1

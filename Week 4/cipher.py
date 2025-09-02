1340a97ef6010366753d4a3c922551283bf34247
Rodrigo <rlopezca89@gmail.com>
2025-09-02 16:40:10 +0000

    class 19

---
+++ Week 4/cipher.py
@@ -0,0 +1,20 @@
+def main():
+#26 letras
+    message = input("Write a messege:").lower()
+    encode_message(message)
+
+def encode_message (text):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    Clipher = "zyxwvutsrqponmlkjihgfedcba"
+    new_message = ""    
+    i = 0
+    
+    while i < len(text):
+        char = text[i]
+
+        
+
+
+
+
+main ()

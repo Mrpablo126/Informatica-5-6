52aff678a086668b3fc1e066aaef1d7e5abc79fa
Rodrigo <rlopezca89@gmail.com>
2025-09-04 16:42:30 +0000

    class 20

--- Week 4/cipher.py
+++ Week 4/cipher.py
@@ -1,18 +1,24 @@
 def main():
-#26 letras
-    message = input("Write a messege:").lower()
-    encode_message(message)
 
-def encode_message (text):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    Clipher = "zyxwvutsrqponmlkjihgfedcba"
-    new_message = ""    
-    i = 0
+    message = input("Write a messege:").lower() #This line is the message
+    encode_message(message) #Is the where is the message
+
+def encode_message (text): # Is defining the encode_message
+    alphabet = "abcdefghijklmnopqrstuvwxyz" #Is the alphabet
+    cipher =   "zyxwvutsrqponmlkjihgfedcba" #is the alphabet,but start from the z to the a
+    new_message = "" #Nothing   
+    i = 0 # is the counting
     
-    while i < len(text):
-        char = text[i]
+    while i < len(text): # The loop for the operation
+        char = text[i] # For counting the characters of the word
 
-        
+        if char in alphabet: # This if protect the character and delete numbers or signs  
+            cipher_index = alphabet.find(char) # This is going to find the character on the word
+            new_message += cipher[cipher_index] # is the new messege 
+        else:
+            new_message += char # This is going to put the other things that are not on the alphabet out.
+        i += 1 #Is going to find all the characters depending of the number of characters.
+    print("Encoded message:" + new_message) #Is the print for the final result
 
 
 

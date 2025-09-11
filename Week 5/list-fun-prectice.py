41ef26d1e2ee5fbe84e58754d9895bd1798852da
Rodrigo <rlopezca89@gmail.com>
2025-09-11 17:30:01 +0000

    class 25

---
+++ Week 5/list-fun-prectice.py
@@ -0,0 +1,20 @@
+   
+def lenght():
+    list1 = []
+    values = int(input("Select a value:"))
+    adds = int(input("Select an add:"))
+    
+    list2_mean = []
+
+def range_of_list():
+    range = min(list)
+
+def values_and_adds(values, adds):
+    values_adds = values, adds
+
+    while values_adds < 0:
+        break
+        
+    if values_adds > 0:
+        print(values, adds)
+        return values_and_adds

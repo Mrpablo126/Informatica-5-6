fdf8e8005d5723191901fee2be5de67ffd503cf3
Rodrigo <rlopezca89@gmail.com>
2025-09-11 16:39:42 +0000

    class 24.1

--- Week 5/radio-playlist.py
+++ Week 5/radio-playlist.py
@@ -1,25 +1,21 @@
 
-weekly_playlist = [
-        "Blinding Lights",
-        "Levitating",
-        "As It Was",
-        "Heat Waves",
-        "Good 4 u"
-    ]
+weekly_playlist = ["Blinding Lights", "Levitating", "As It Was", "Heat Waves", "Good 4 u"]
 weekly_playlist.append(' "Drivers License"')
 weekly_playlist.insert(0,"Bohemian Rhapsody")
 weekly_playlist.remove("Good 4 u")
 weekly_playlist.index("Levitating")
 
-
-
-
 print(weekly_playlist)
+
 print(weekly_playlist.count("As It Was"))
-weekly_playlist.sort()
 
+weekly_playlist.reverse()
 print(weekly_playlist)
 
+
+
+weekly_playlist.sort()
+print(weekly_playlist)
 
 
 

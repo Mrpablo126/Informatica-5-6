3916820817758abaddc8ba6c84208b3f897ffdc3
Rodrigo <rlopezca89@gmail.com>
2025-09-11 20:16:33 +0000

    class 26.2

--- Week 5/list-fun-prectice.py
+++ Week 5/list-fun-prectice.py
@@ -1,6 +1,7 @@
 def main():  
-    values()
-
+    print(length(values()))
+    mean(values())
+    range_of_list(values())
 def values():
     value_list = []
     while True:
@@ -10,11 +11,19 @@ def values():
             print(value_list) 
             ordered_list = sorted(value_list)
             print(ordered_list)
+            
             continue
         else:
             break
-    
-        
+    return value_list
 
+def length(list):
+    return len(list)
+
+def mean(list):
+    print(sum(list) / len(list))
+
+def range_of_list(list):
+    print(max(list) - min(list))    
 
 main()

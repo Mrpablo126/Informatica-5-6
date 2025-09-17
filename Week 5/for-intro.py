360c30e04b902e3a686e7c46363a322731d2f16f
Rodrigo <rlopezca89@gmail.com>
2025-09-17 20:20:04 +0000

    class 27

---
+++ Week 5/for-intro.py
@@ -0,0 +1,12 @@
+doctrine = ["faith", "repentance", "baptism", "Holy Ghost", "Endure to the end"]
+
+# i = 0
+# while i < len(doctrine):
+#     print(doctrine[i])
+#     i += 1
+
+
+for principle in doctrine:
+    print(principle)
+
+school_objective = ["Deepen conversion to Jesus Christ and His restored Gospel", "Qualify for the blessings of the Temple", "Prepare for missionary and Chruch service", "Build the kingdom of God"]

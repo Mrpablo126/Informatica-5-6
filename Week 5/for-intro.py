d221936ba6ad047f5df03ced0fccea139f2ac21d
Rodrigo <rlopezca89@gmail.com>
2025-09-19 17:25:32 +0000

    class 27

--- Week 5/for-intro.py
+++ Week 5/for-intro.py
@@ -10,3 +10,7 @@ for principle in doctrine:
     print(principle)
 
 school_objective = ["Deepen conversion to Jesus Christ and His restored Gospel", "Qualify for the blessings of the Temple", "Prepare for missionary and Chruch service", "Build the kingdom of God"]
+
+for object in school_objective:
+    print(object)
+    print("😎😎")

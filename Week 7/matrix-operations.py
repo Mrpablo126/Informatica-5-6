87aea089aefd2a7bc7dda9e7a39be3a0e95a6b66
Rodrigo <rlopezca89@gmail.com>
2025-09-24 20:15:34 +0000

    class 30

--- Week 7/matrix-operations.py
+++ Week 7/matrix-operations.py
@@ -1,9 +1,10 @@
-def sum_of_row(matrix, row_number: int):
-    row = matrix [row_number]
-    row_sum = 0
-    for item in row:
-        #row_sum = row_sum + item #linea 5 y 6 es lo mismo
-        row_sum += item          
+def sum_of_row(matrix, row_number):
+    return sum(matrix[row_number])
+    # row = matrix [row_number]
+    # row_sum = 0
+    # for item in row:
+    #     #row_sum = row_sum + item #linea 5 y 6 es lo mismo
+    #     row_sum += item          
 
     return row_sum
 
@@ -14,6 +15,13 @@ def sum_of_column(matrix, column_number: int):
     
     return column_sum
 
+number = int(input("Select a number:"))
+
 m = [[4,2,3,2], [9,1,12,11], [7,8,9,5], [2,9,15,1]]
 my_sum = sum_of_column(m,2)
 print(my_sum)
+m[0][3]= number
+print(m)
+
+print(sum_of_column(m,1))
+

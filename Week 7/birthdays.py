4ec5b5dd3b292e4faf28412106429eba37c1201d
Rodrigo <rlopezca89@gmail.com>
2025-09-27 05:55:39 +0000

    class 31.2

--- Week 7/birthdays.py
+++ Week 7/birthdays.py
@@ -1,11 +1,25 @@
-birthdays = {
-   "Jose": "19/05/2008",
-   "Noemi": "09/12/2008",
-   "Luna": "22/01/2008"
-}
-print("1-Jose\n""2-Noemi\n""3-Luna")
-name = input("Enter a name:").title()
-if name in birthdays:
-   print(f"The birthday of",name, "is:",birthdays[name])
-else:
-   print("This name is not in the list")
+def main():
+   birthdays = {
+      "Jose": "19/05/2008",
+      "Noemi": "09/12/2008",
+      "Luna": "22/01/2008"
+   }
+   print("1-Jose\n""2-Noemi\n""3-Luna")
+   name = input("Enter a name:").title()
+
+
+   while True:
+      a = 0
+      if name in birthdays:
+         print(f"The birthday of",name, "is:",birthdays[name])
+         break
+      if name not in birthdays:
+         print("This name is not in the list")
+         m = input("Enter the new name:")
+         n = input("Enter the birthday:")
+         birthdays[m] = n
+         print("Birthday added")
+         print(birthdays)
+   return 
+         
+main()  
--- Week 7/dictionaries.py
+++ Week 7/dictionaries.py
@@ -4,8 +4,10 @@ capitals = {
     "Brazil": "Brasilia",
     # "Canada": "Montreal" #Use unique keys
 }
+
 capitals["Italy"] = "Rome" #Add a new key and value
 del capitals["Brazil"] #Deletes key and value
+print(capitals)
 capitals.pop("Canada") #Is the same that line 8
 capitals.clear()
 
@@ -20,11 +22,11 @@ capitals.clear()
 #     s = Houses[student]
 #     print(f"{student}: {s}")
 
-students =[
-    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
-    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
-    {"name": "Draco", "house": "Slytherin", "patronus": None}
-]
-for element in students:
-    print(f" {element["name"]}, {element["house"]}, {element["patronus"]}")
+# students =[
+#     {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+#     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+#     {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+#     {"name": "Draco", "house": "Slytherin", "patronus": None}
+# ]
+# for element in students:
+#     print(f" {element["name"]}, {element["house"]}, {element["patronus"]}")

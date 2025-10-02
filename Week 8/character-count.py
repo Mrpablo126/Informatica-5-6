2961982a02e1dac9083c2d39f0295cf4f18762f1
Rodrigo <rlopezca89@gmail.com>
2025-10-02 15:42:45 +0000

    class 35

--- Week 8/character-count.py
+++ Week 8/character-count.py
@@ -6,6 +6,16 @@ def main():
             dictionary[character] += 1
 
         print(dictionary)
+        values_list = list(dictionary.values())
+        print(values_list)
+        largest_number_index = values_list.index(max(values_list))
+        repeated_character = list(dictionary.keys())[largest_number_index]
+        print(f"The most repeteated character is: {repeated_character}, repeating {dictionary[repeated_character]}")
+
+        # print(sum(dictionary.values())) es lo mismo que la linea 21
+        #Alternativa 2
+        largest_number2 = max(dictionary, key=dictionary.get)
+        print(f"The most repeteated character is:{largest_number2}, repeating {dictionary[repeated_character]}")
         
     message = input("Write a message:")
     dictionary = {}

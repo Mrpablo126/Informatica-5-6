7f14540432ca610a26f003e66f827d80680cc911
Rodrigo <rlopezca89@gmail.com>
2025-10-06 20:18:56 +0000

    class 37

--- Week 8/Scrabble.py
+++ Week 8/Scrabble.py
@@ -1,13 +1,16 @@
 import random 
-def main(words):
-    dictionary = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
-    
-    print("Welcome to Scrabble")
-    print("Your letters are:")
-   
-def words_in_Scrabble(dictionary, words):
-    words = random.randint(dictionary,13)
-    print(words)
-    words_in_Scrabble(dictionary,words)
+import string
 
-main()
+alphabet = {"a": 1,"b": 3,"c": 3,"d": 2,"e": 1,"f": 4,"g": 2,"h": 4,"i": 1,"j": 8,"k": 5,"l": 1,"m": 3,"n": 1,"o": 1,"p": 3,"q":10,"r": 1,"s": 1,"t": 1,"u":1,"v": 4,"w": 4,"x": 8,"y": 4,"z": 10}
+
+print("Welcome to Scrabble")
+print("Your letters are:")
+   
+user_letters = []
+i = 0
+while i < 13:
+    i += 1
+    num = random.choice(string.ascii_uppercase)
+    user_letters.append(num)
+print(user_letters)     
+

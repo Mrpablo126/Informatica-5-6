15ea0a73fa660c7172e90345832e640a552d966f
Rodrigo <rlopezca89@gmail.com>
2025-10-30 17:29:23 +0000

    class 44

---
+++ Week 10/factorial.py
@@ -0,0 +1,12 @@
+def main():
+    a = int(input("Select a positive integer:"))
+  
+    while a == 1:
+        list = []
+        list.append(a)
+        a -= 1
+        
+print(list)
+main()
+
+
--- Week 10/fuel.py
+++ Week 10/fuel.py
@@ -1,20 +1,29 @@
 def main():
-    
-    while True:
-        
-        try:
-            fuel = input("Write a fraction:").split("/")
-            print(fuel)
-            fuel_porcent = (int(fuel[0]) / int(fuel[1])) * 100
-            if fuel[0] == fuel[1]:
-                print("F")
-            elif fuel == 0/fuel[1]:
-                print("E")
-            else:
-                print(fuel_porcent)
-                
-        except:
-            print("Try again")
-            
+    fuel()
 
+def fuel():
+    condition = True
+    while condition:
+        try:
+            fraction = input("Fuel fraction:").split("/")
+            num = int(fraction[0])
+            den = int(fraction[1])
+            percentage = round((num/den)*100)
+            if percentage > 100:
+                print("Invalid input. Fuel tank cannot be filled more than 100%")
+            elif percentage >= 99:
+                print("F")
+                condition = False
+            elif percentage <= 1:
+                print("E")
+                condition = False
+            else:
+                print(f"The percentage is: {percentage}%")
+                condition = False
+        except (ZeroDivisionError, IndexError):
+            print("Invalid fraction")
+        except ValueError:
+            print("Please type numbers instead of words.")
+        
+            
 main()

503efebfac7c12a6b2f632be2b53ce49c825b378
Rodrigo <rlopezca89@gmail.com>
2025-11-10 19:39:50 +0000

    class 50

--- Week 10/factorial.py
+++ Week 10/factorial.py
@@ -2,11 +2,9 @@
 print("Type a positive integer:")
 x = int(input())
 fact = 1
-for i in range(1, x + 1):
-    fact *= i
-    while x < 0:
-        print("ERROR")
-        return 
-print("The factorial of", x ,"is: ", fact)
-
-
+if x > 0:
+    for i in range(1, x + 1):
+        fact *= i
+    print("The factorial of", x ,"is: ", fact)
+else:
+    print("ERROR")

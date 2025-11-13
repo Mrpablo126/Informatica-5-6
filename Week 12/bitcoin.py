3ca355d55b3f9b7324dd5ab60048385b16ee4099
Rodrigo <rlopezca89@gmail.com>
2025-11-13 20:19:56 +0000

    class 52

--- Week 12/bitcoin.py
+++ Week 12/bitcoin.py
@@ -1,6 +1,9 @@
 import requests
 API_KEY = "a1450b56fa14a82f77d708b098d40d9a9a7894a49b42bfb7eb8b94d5346e3f31"
 
-bitcoin = float(input("How mny bitcoins do you want to buy?"))
+bitcoin = float(input("How many bitcoins do you want to buy?"))
  
 response = requests.get("http://rest.coincap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+bitcoin_price = float(response.json()["data"]["priceUsd"])
+total = round(bitcoin * bitcoin_price, 2)
+print(f"Your total will be: ${total:,.2f}")
---
+++ Week 12/problem-api.py
@@ -0,0 +1,4 @@
+import requests
+
+l = requests.get("https://api.hackerearth.com/v4/partner/code-evaluation/submissions/lang")
+response.json()["data"]["priceUsd"])
